6bbba13d8d507b45520b2c859b94a5cf418c39db
Za <Zubuhle01@gmail.com>
2026-08-08 21:04:16 +0200

    Added button controller to the buttons
    _The butttons do detect the input system keys. But it looks like they detect it once and then after they do not respond. This causes the when

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ButtonController : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public InputActionReference keyToPress;
+  
+    private void Awake()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    
+    private void OnEnable()
+    {
+        keyToPress.action.Enable();
+
+        keyToPress.action.performed += OnKeyPressed;
+        keyToPress.action.canceled -= OnKeyReleased;
+
+    }
+
+    private void OnDisable()
+    {
+        keyToPress.action.performed -= OnKeyPressed;
+        keyToPress.action.canceled -= OnKeyReleased;
+
+        keyToPress.action.Disable();
+    }
+
+    private void OnKeyPressed (InputAction.CallbackContext context)
+    {
+        theSR.sprite = pressedImage;
+    }
+
+    private void OnKeyReleased(InputAction.CallbackContext context)
+    {
+        theSR.sprite = defaultImage;
+    }
+}
