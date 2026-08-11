a9e1f8465e2c8ecd390175221d6ef403ac8fa4a9
Amymaara <98781672+Amymaara@users.noreply.github.com>
2026-08-11 17:56:47 +0200

    More scripts and setup (Feedback)

--- Assets/Scripts/Score System/ScoreUI.cs
+++ Assets/Scripts/Score System/ScoreUI.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour
 {
@@ -9,6 +10,20 @@ public class ScoreUI : MonoBehaviour
     [SerializeField] TMP_Text comboText;
     [SerializeField] TMP_Text accuracyText;
     [SerializeField] Slider healthBar;
+    [SerializeField] Image judgementImage;
+    [SerializeField] CanvasGroup judgementCanvasGroup;
+
+    [Header("Sprites")]
+    public Sprite perfectSprite;
+    public Sprite goodSprite;
+    public Sprite okSprite;
+    public Sprite missSprite;
+
+    public float displayTime = 0.3f;
+
+    public float fadeTime = 0.2f;
+
+    private Coroutine judgementCoroutine;
 
 
     private void Update()
@@ -26,5 +41,66 @@ public class ScoreUI : MonoBehaviour
     public void UpdateHealthBar(float health)
     {
        // healthBar.value = currentHealth;
+    }
+
+    public void ShowJudgement(HitJudgement judgement)
+    {
+        switch (judgement)
+        {
+            case HitJudgement.Perfect:
+                judgementImage.sprite = perfectSprite;
+                break;
+
+            case HitJudgement.Good:
+                judgementImage.sprite = goodSprite;
+                break;
+
+            case HitJudgement.Ok:
+                judgementImage.sprite = okSprite;
+                break;
+
+            case HitJudgement.Miss:
+                judgementImage.sprite = missSprite;
+                break;
+        }
+
+        if (judgementCoroutine != null)
+        {
+            StopCoroutine(judgementCoroutine);
+        }
+
+        judgementCoroutine = StartCoroutine(DisplayJudgement());
+    }
+
+    private IEnumerator DisplayJudgement()
+    {
+        judgementImage.gameObject.SetActive(true);
+       
+
+        judgementCanvasGroup.alpha = 1f;
+       
+
+        yield return new WaitForSeconds(displayTime);
+        
+
+
+        float timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+
+            judgementCanvasGroup.alpha =
+                Mathf.Lerp(1f, 0f, timer / fadeTime);
+
+         
+
+            yield return null;
+        }
+
+
+        judgementCanvasGroup.alpha = 0f;
+        judgementImage.gameObject.SetActive(false);
+    
     }
 }
